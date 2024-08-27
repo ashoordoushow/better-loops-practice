feed0f6e07c3b97b0256b22087052d68cf74a386
ashoordoushow <ashoor12323@gmail.com>
2024-08-27 01:31:08 -0500

    print out elements in array using while loop

---
+++ while_vs_each.rb
@@ -0,0 +1,17 @@
+# print out each element in this array
+
+fruit = ["apple", "banana", "avacado", "peaches"]
+
+p fruit
+
+# while loop
+i = 0
+
+while i < fruit.length
+    p fruit[i] #for this line to work had to change number '10' to 'fruit.length' in order to read '[i]'
+    i += 1
+end
+
+
+
+# each loop
